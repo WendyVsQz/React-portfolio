11f8a40e090f85e9b1d3fe7bf1eaf4ea4510c837
Wendy <118736912+WendyVsQz@users.noreply.github.com>
2023-03-12 19:25:33 +0000

    navbar components done

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -4,14 +4,23 @@ import {HiHomeModern} from 'react-icons/hi2'
 import {FaUserCircle} from 'react-icons/fa'
 import{IoCodeWorkingSharp} from 'react-icons/io5'
 import {AiFillMessage} from 'react-icons/ai'
+import { useState } from 'react'
 
-export const navbar = () => {
+const navbar = () => {
+  const {activeNav, setActiveNav} = useState('#')
   return (
-    <nav>
-      <a href='#' className='active'><HiHomeModern/></a> 
-      <a href='#about'><FaUserCircle/></a> 
-      <a href='#portfolio'><IoCodeWorkingSharp/></a> 
-      <a href='#contact'><AiFillMessage/></a> 
+    <nav> {/* home section */}
+      <a href='#' onClick={()=> setActiveNav('#')} 
+      className= {activeNav ==='#' ? 'active' : ''}><HiHomeModern/></a> 
+      {/* about section */}
+      <a href='#about' onClick={() => setActiveNav('#about')} 
+      className= {activeNav ==='#' ? 'active' : ''}><FaUserCircle/></a> 
+      {/* portfolio section*/}
+      <a href='#portfolio' onClick={()=> setActiveNav('#portfolio')} 
+      className={activeNav === '#' ? 'active' : ''}><IoCodeWorkingSharp/></a> 
+      {/* contact section */}
+      <a href='#contact'onClick={()=> setActiveNav('#contact')} 
+      className={activeNav === '#' ? 'active' : ''}><AiFillMessage/></a> 
     </nav>
   )
 }
