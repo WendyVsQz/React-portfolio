dd28223d74d74c26cc10a1bdef24fae1041f6d4b
Wendy <118736912+WendyVsQz@users.noreply.github.com>
2023-03-13 18:51:44 +0000

    responsive cards

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -6,6 +6,40 @@ import project3 from '../../Assets/password generator.jpg'
 import project4 from '../../Assets/Readme generator.png'
 import project5 from '../../Assets/Group profile generator.png'
 
+const data=[
+  {
+    id: 1,
+    image: project1,
+    tittle: 'Weather dashboard',
+    github: 'https://github.com/WendyVsQz/Weather-dashboard'
+  },
+  {
+    id: 2,
+    image: project2,
+    tittle: 'Work day planner',
+    github: 'https://github.com/WendyVsQz/Work-day-planner'
+  },
+  {
+    id: 3,
+    image: project3,
+    tittle: 'password generator',
+    github: 'https://github.com/WendyVsQz/Password-generator'
+  },
+  {
+    id: 4,
+    image: project4,
+    tittle: 'Readme generator',
+    github: 'https://github.com/WendyVsQz/Readme-generator'
+  },
+  {
+    id: 5,
+    image: project5,
+    tittle: 'Group profile generator',
+    github: 'https://github.com/WendyVsQz/Team-profile-generator'
+  },
+
+]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -13,48 +47,24 @@ const Portfolio = () => {
     <h2> Portfolio </h2>
 
   <div className='container portfolio_container'>
-  <article className='portfolio_item'> 
+    {
+      data.map(({id, image, title, github}) => {
+        return(
+  <article key={id} className='portfolio_item'> 
   <div className='portfolio_item-image'>
-    <h3> Weather dashboard</h3>
-      <a href='https://github.com/WendyVsQz/Weather-dashboard'className='btn'>Github</a>
-      <img src={project1} alt='weather dashboard'></img>
+      <img src={image} alt={title}/>
   </div>
+    <h3> {title}</h3>
+    <div className='portfolio_item-cta'>
+      <a href={github}className='btn btn-primary' target='_blank'>Github</a>
+    </div>
   </article>
-
-  <article className='portfolio_item'>
-  <div className='portfolio_item-image'>
-    <h3> Work day planner </h3>
-      <a href='https://github.com/WendyVsQz/Work-day-planner'className='btn'>Github</a>
-      <img src={project2} alt='Work day planner'></img>
+          
+        )
+      })
+    }
   </div>
-  </article>
-
-  <article className='portfolio_item'>
-  <div className='portfolio_item-image'>
-    <h3> password generator </h3>
-      <a href='https://github.com/WendyVsQz/Password-generator'className='btn'>Github</a>
-      <img src={project3} alt='password generator'></img>
-  </div>
-  </article>
-
-  <article className='portfolio_item'>
-  <div className='portfolio_item-image'>
-    <h3> Readme generator </h3>
-      <a href='https://github.com/WendyVsQz/Readme-generator'className='btn'>Github</a>
-      <img src={project4} alt='Readme generator'></img>
-  </div>
-  </article>
-
-  <article className='portfolio_item'>
-  <div className='portfolio_item-image'>
-    <h3> Group profile generator</h3>
-      <a href='https://github.com/WendyVsQz/Team-profile-generator'className='btn'>Github</a>
-      <img src={project5} alt='Group profile generator'></img>
-  </div>
-  </article>
-</div>
-
-    </section>
+</section>
 )
 }
 
