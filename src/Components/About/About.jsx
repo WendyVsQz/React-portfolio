ba58f9e5562eac2751473b5a00725b431735899a
Wendy <118736912+WendyVsQz@users.noreply.github.com>
2023-03-13 19:57:20 +0000

    about component done

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,29 +7,22 @@ export const About = () => {
     <section id ="about">
 <h5>Get to know me</h5>
 <h1>About me </h1>
+
 <div className='container about_container'>
 <div className='about_me'>
+  <div className='about_me-image'>
         <img src={ME} alt="About image"/>
       </div>
 </div>
-<div className='about_text'>
-<p>
-"Sed ut perspiciatis unde omnis iste natus error sit voluptatem 
-accusantium doloremque laudantium,totam rem aperiam, eaque ipsa
-quae ab illo inventore veritatis et quasi architecto beatae vitae
-dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit 
-aspernatur aut odit aut fugit, sed quia consequuntur magni dolores 
-eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, 
-qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
-sed quia non numquam eius modi tempora incidunt ut labore et dolore
-magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis 
-nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut 
-aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit
-qui in ea voluptate velit esse quam nihil molestiae consequatur, vel 
-illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-</p>
+  <p>
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+Eos, accusantium quas. Aliquid maiores perspiciatis id doloremque fuga, 
+sequi ipsa neque voluptatibus cumque alias consectetur dolore recusandae qui quam molestiae! 
+Asperiores?
+  </p>
+<a href='#contact' className='btn'>get in contact</a>
 </div>
-    </section>
+</section>
   )
 }
 
